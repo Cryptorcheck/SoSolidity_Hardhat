61d4a012a6648f28ae9e3b4d2009fc397fd755f4
Ivan <243979384@qq.com>
2025-09-24 14:52:28 +0800

    test tx

--- 03_Hardhat/contracts/MyToken.sol
+++ 03_Hardhat/contracts/MyToken.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.28;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
+    address public owner;
+
     constructor(uint initialSupply) ERC20("MyToken", "MTK") {
+        owner = msg.sender;
         _mint(msg.sender, initialSupply);
     }
 }
--- 03_Hardhat/test/MyToken.ts
+++ 03_Hardhat/test/MyToken.ts
@@ -4,14 +4,12 @@ import { ethers } from "hardhat";
 
 describe("", () => {
   let contract: BaseContract;
+  // MyToken合约的构造函数参数
+  const initialSupply = 1000000;
 
   beforeEach(async () => {
     const MyToken = await ethers.getContractFactory("MyToken");
 
-    // MyToken合约的构造函数参数
-    const initialSupply = 1000000;
-
-    
     // 使用deploy方法，不指定部署账号直接部署合约
     // 默认会使用hardhat中的测试account1
     // 可通过 await ethers.getSigners() 获取hardhat中的测试账号列表
@@ -27,7 +25,8 @@ describe("", () => {
     expect(addr).to.length.greaterThan(0);
   });
 
-  it("验证合约name,symbol,decimals", async() => {
+  it("验证合约name,symbol,decimals并测试转帐", async() => {
+    console.log("\n验证合约数据");
     // name、symbol、decimals都是ERC20的内置方法
     const name = await (contract as any).name();
     console.log("name: ", name);
@@ -39,9 +38,22 @@ describe("", () => {
     expect(name).to.equal("MyToken");
     expect(symbol).to.equal("MTK");
     expect(decimals).to.equal(18);
+
+    console.log("\n测试转帐");
+    // 在部署合约时会mint数额为initialSupply的代币到部署合约的账户
+    // 现在来验证一下
+    const [acc01] = await ethers.getSigners();
+    const balanceOfAcc01 = await (contract as any).balanceOf(acc01);
+    expect(balanceOfAcc01).to.equal(initialSupply);
+
+    // 检查MyToken的owner是否等于hardhat的第一个测试账户
+    const owner = await (contract as any).owner();
+    console.log("owner: ",owner);
+    console.log("acc01: ",acc01.address);
+    expect(acc01.address).to.equal(owner);
   })
 
-  it("测试转帐", () => {
-    console.log("测试转帐");
-  })
+  // it("测试转帐", async () => {
+    
+  // })
 });
